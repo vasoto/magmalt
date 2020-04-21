b5ced43633d2f954f4694b8bd5348792ebd34711
Vassil Verguilov <Vassil.Verguilov@gmail.com>
2020-04-21 13:09:37 +0300

    Keras model is defined as Sequential model instead of the functional approach

--- src/magmalt/models/keras/simple.py
+++ src/magmalt/models/keras/simple.py
@@ -1,12 +1,10 @@
 import logging
 
-try:
-    from tensorflow import keras
-except Exception as _:
-    import keras
-    logging.warning('Upgrade Tensorflow!')
+from tensorflow import keras
 
 from magmalt.core import Model
+
+logger = logging.getLogger('keras_simple_model')
 
 
 class KerasSimpleModel(Model):
@@ -36,33 +34,51 @@ class KerasSimpleModel(Model):
         self.use_batch_normalization = use_batch_normalization
         self.dropout_rate = dropout_rate
 
-    def _create_layer(self, prev_layer, layer_index, size):
-        x = keras.layers.Dense(units=size,
-                               activation=self.hidden_activation,
-                               kernel_initializer=self.kernel_initializer,
-                               name=f'Layer_{layer_index + 1}')(prev_layer)
-        if self.use_batch_normalization:
-            x = keras.layers.BatchNormalization(
-                name=f'NormalizationLayer_{layer_index + 1}')(x)
-        if 0.0 < self.dropout_rate < 1.0:
-            x = keras.layers.Dropout(rate=self.dropout_rate,
-                                     name=f"Dropout_{layer_index + 1}")(x)
-        return x
-
     def create(self):
+        """ Create the model
+        """
         input_dim = len(self.inputs)
-        inputs = keras.layers.Input(shape=(input_dim), name='Inputs')
-        x = inputs
+        output_dim = len(self.outputs)
+        # Simple, sequential model
+        model = keras.Sequential()
+        # Create input layer
+        model.add(keras.layers.Input(shape=(input_dim, ), name='Inputs'))
         if self.use_batch_normalization:
-            # Add batch normalization after the input layer
-            x = keras.layers.BatchNormalization(name='NormalizationLayer')(x)
+            model.add(
+                keras.layers.BatchNormalization(name='Inputs_Normalization'))
+        logger.debug('Add input layer with %d inputs to model %s', input_dim,
+                     self.name)
+        # Add hidden layers
         for i, layer_size in enumerate(self.layers):
-            x = self._create_layer(x, i, layer_size)
-
-        # Add output
-        outputs = keras.layers.Dense(
-            len(self.outputs),
-            activation=self.output_activation,
-            kernel_initializer=self.kernel_initializer,
-            name='Output')(x)
-        self.model = keras.models.Model(inputs=[inputs], outputs=[outputs])
+            # Hidden layer
+            model.add(
+                keras.layers.Dense(layer_size,
+                                   activation=self.hidden_activation,
+                                   kernel_initializer=self.kernel_initializer,
+                                   name=f"Layer_{i + 1}"))
+            logger.debug('Add hidden layer #%d with %d units for model %s',
+                         i + 1, layer_size, self.name)
+            # Batch normalization
+            if self.use_batch_normalization:
+                model.add(
+                    keras.layers.BatchNormalization(
+                        name=f'Layer_{i + 1}_Normalization'))
+                logger.debug(
+                    'Add batch normalization after hidden layer #%d for model %s',
+                    i + 1, self.name)
+            # Dropout rate
+            if 0.0 < self.dropout_rate < 1.0:
+                model.add(
+                    keras.layers.Dropout(rate=self.dropout_rate,
+                                         name=f"Dropout_{i + 1}"))
+                logger.debug(
+                    'Add dropout after layer #%d with probability %f for model %s',
+                    i + 1, self.dropout_rate, self.name)
+        # Add output layer
+        model.add(
+            keras.layers.Dense(output_dim,
+                               activation=self.output_activation,
+                               name='Output'))
+        logger.debug('Add output layer %d outputs for model %s', output_dim,
+                     self.name)
+        self.model = model
