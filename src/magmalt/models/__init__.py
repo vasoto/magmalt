d79a2d6596dd4b1182571b0df363c4f8e1a50fc7
Vassil Verguilov <Vassil.Verguilov@gmail.com>
2020-04-21 13:11:13 +0300

    Fix profiling

--- src/magmalt/models/__init__.py
+++ src/magmalt/models/__init__.py
@@ -1,4 +1,4 @@
-from .model_builder import ModelBuilder
-from .keras.simple import KerasSimpleModel
-from .xgboost.xgboost_models import XGBoostRegressorModel
-from .trainers.keras_trainer import KerasModelTrainer
+from .model_builder import ModelBuilder  # pragma: no coverage
+from .keras.simple import KerasSimpleModel  # pragma: no coverage
+from .xgboost.xgboost_models import XGBoostRegressorModel  # pragma: no coverage
+from .trainers.keras_trainer import KerasModelTrainer  # pragma: no coverage
